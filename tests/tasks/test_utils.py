b54345dc8eabb684c4e2542f62395bc2bc9c31a0
Iain Dillingham <iain@dillingham.me.uk>
2025-06-25 14:22:58 +0100

    Test `tasks.utils.get_engine`

--- tests/tasks/test_utils.py
+++ tests/tasks/test_utils.py
@@ -1,9 +1,12 @@
 import pytest
 
+from tasks import utils
 
-@pytest.mark.xfail
-def test_get_engine():
-    assert False
+
+def test_get_engine(monkeypatch):
+    monkeypatch.setenv("JOBSERVER_DATABASE_URL", "sqlite+pysqlite:///:memory:")
+    engine = utils.get_engine()
+    assert str(engine.url) == "sqlite+pysqlite:///:memory:"
 
 
 @pytest.mark.xfail
