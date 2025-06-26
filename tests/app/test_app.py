0062dc0e4c0ef01530619444bedf1aafb96fe7d7
Iain Dillingham <iain@dillingham.me.uk>
2025-06-26 07:33:13 +0100

    Test Streamlit app
    This tests the entire Streamlit app, using Streamlit's `AppTest` class.
    To decouple the app from the data, we monkeypatch `DATA_DIR`.

---
+++ tests/app/test_app.py
@@ -0,0 +1,17 @@
+from streamlit.testing.v1 import AppTest
+
+import tasks
+
+
+def test_app(tmp_path, monkeypatch):
+    tmp_f_path = tmp_path / "job_requests" / "job_requests.csv"
+    tmp_f_path.parent.mkdir()
+    tmp_f_path.write_text(
+        "created_at,num_actions,num_jobs\n2025-01-01 00:00:00.0+00:00,1,1\n"
+    )
+    monkeypatch.setattr(tasks, "DATA_DIR", tmp_path)
+
+    app_test = AppTest.from_file(tasks.ROOT_DIR / "app" / "app.py")
+    app_test.run()
+
+    assert not app_test.exception
