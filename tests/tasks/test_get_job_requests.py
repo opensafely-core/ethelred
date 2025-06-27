abcbcdc886bb3c4efdea75f56a3d26e9f9f2c1e8
Iain Dillingham <iain@dillingham.me.uk>
2025-06-27 06:10:23 +0100

    Remove `test_get_record`
    `get_record` is also covered by `test_transform` so we can safely remove
    it.

--- tests/tasks/test_get_job_requests.py
+++ tests/tasks/test_get_job_requests.py
@@ -19,20 +19,6 @@ def test_load_project_definition(tmp_path):
     assert project_definition == {}
 
 
-def test_get_record():
-    row = Row(
-        "https://github.com/opensafely/my-repo",
-        "0000000",
-        datetime.datetime(2025, 1, 1),
-        1,
-    )
-    project_definition = {"actions": {"a1": {}, "a2": {}}}
-    record = get_job_requests.get_record(row, project_definition)
-    assert record.created_at == datetime.datetime(2025, 1, 1)
-    assert record.num_actions == 2
-    assert record.num_jobs == 1
-
-
 def test_transform():
     row = Row(
         "https://github.com/opensafely/my-repo",
