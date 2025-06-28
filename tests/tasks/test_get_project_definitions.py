b225f639157f573d558b8f0f033957cdfe1cbbcd
Iain Dillingham <iain@dillingham.me.uk>
2025-06-28 06:43:44 +0100

    Test `tasks.get_project_definitions.write`

--- tests/tasks/test_get_project_definitions.py
+++ tests/tasks/test_get_project_definitions.py
@@ -1,4 +1,5 @@
 import collections
+import pickle
 
 from tasks import get_project_definitions
 
@@ -20,3 +21,17 @@ def test_get_record():
     assert record.repo == "my-repo"
     assert record.sha == "0000000"
     assert record.project_definition == {"actions": {"a1": {}, "a2": {}}}
+
+
+def test_write(tmp_path):
+    record = get_project_definitions.Record(
+        "my-repo", "0000000", {"actions": {"a1": {}, "a2": {}}}
+    )
+    project_definitions_dir = tmp_path / "project_definitions"
+
+    get_project_definitions.write(record, project_definitions_dir)
+
+    with open(project_definitions_dir / "my-repo" / "0000000.pickle", "rb") as f:
+        project_definition = pickle.load(f)
+
+    assert project_definition == {"actions": {"a1": {}, "a2": {}}}
