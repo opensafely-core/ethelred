opensafely-core/ethelred

commit fc24691f96687f9f4df54c8355f6573a1701859c

Decouple `app` from `tasks`
The small amount of duplication (`ROOT_DIR`, `DATA_DIR`) ensures that
the modules are decoupled.

app/app.py
@@ -1,8 +1,13 @@
+import pathlib
+
 import altair
 import pandas
 import streamlit
 
-from tasks import DATA_DIR
+
+ROOT_DIR = pathlib.Path(__file__).parents[1]
+
+DATA_DIR = ROOT_DIR / "data"
 
 
 def get_job_requests(f_path):

tests/app/test_app.py
@@ -1,6 +1,6 @@
 from streamlit.testing.v1 import AppTest
 
-import tasks
+from app import app
 
 
 def test_app(tmp_path, monkeypatch):
@@ -9,9 +9,10 @@ def test_app(tmp_path, monkeypatch):
     tmp_f_path.write_text(
         "created_at,num_actions,num_jobs\n2025-01-01 00:00:00.0+00:00,1,1\n"
     )
-    monkeypatch.setattr(tasks, "DATA_DIR", tmp_path)
 
-    app_test = AppTest.from_file(tasks.ROOT_DIR / "app" / "app.py")
+    monkeypatch.setattr(app, "DATA_DIR", tmp_path)
+
+    app_test = AppTest.from_file(app.ROOT_DIR / "app" / "app.py")
     app_test.run()
 
     assert not app_test.exception

app/__init__.py
@@ -1,0 +1,1 @@
+# Don't put anything here that you want to import from a Streamlit app.

commit 9eea12ef239ada281d00b88dca47ecd37e306d77

Get `DATA_DIR` from environment
This fixes a regression introduced by fc24691. To decouple `app` from
`tasks` *and* to test `app`, it's necessary to get the `DATA_DIR` from
the environment. Ultimately, this is because Streamlit doesn't load
`app` as a Python module.

app/app.py
@@ -1,3 +1,4 @@
+import os
 import pathlib
 
 import altair
@@ -5,9 +6,7 @@ import pandas
 import streamlit
 
 
-ROOT_DIR = pathlib.Path(__file__).parents[1]
-
-DATA_DIR = ROOT_DIR / "data"
+DATA_DIR = pathlib.Path(os.environ["DATA_DIR"])
 
 
 def get_job_requests(f_path):

dotenv
@@ -1,1 +1,2 @@
+DATA_DIR="data"
 JOBSERVER_DATABASE_URL=postgresql://user:pass@localhost:6543/jobserver

tests/app/test_app.py
@@ -1,7 +1,5 @@
 from streamlit.testing.v1 import AppTest
 
-from app import app
-
 
 def test_app(tmp_path, monkeypatch):
     tmp_f_path = tmp_path / "job_requests" / "job_requests.csv"
@@ -9,10 +7,9 @@ def test_app(tmp_path, monkeypatch):
     tmp_f_path.write_text(
         "created_at,num_actions,num_jobs\n2025-01-01 00:00:00.0+00:00,1,1\n"
     )
+    monkeypatch.setenv("DATA_DIR", str(tmp_path))
 
-    monkeypatch.setattr(app, "DATA_DIR", tmp_path)
-
-    app_test = AppTest.from_file(app.ROOT_DIR / "app" / "app.py")
+    app_test = AppTest.from_file("app/app.py")
     app_test.run()
 
     assert not app_test.exception