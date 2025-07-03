cd1576c9fac6685d43a67981c28671eb0db1b824
Alice Wong <alice.wong@thedatalab.org>
2025-07-03 14:42:32 +0100

    Extract id and job request id in `get_jobs` task
    - In the `get_jobs` task, connect to the database and extract the
    job id and job request id as columns.
    These columns are directly loaded into `jobs.csv`.
    
    - The extraction query uses the `created_at` of the job request to
    filter jobs to be consistent with the other tasks, at a small
    cost in query performance.

--- tests/tasks/test_get_jobs.py
+++ tests/tasks/test_get_jobs.py
@@ -3,11 +3,13 @@ import collections
 from tasks import get_jobs
 
 
-Row = collections.namedtuple("Row", [])
+Row = collections.namedtuple("Row", ["id", "job_request_id"])
 
 
 def test_transform():
-    rows = [Row()]
+    rows = [Row(123, 4567)]
     records = list(get_jobs.transform(rows))
     record = records[0]
-    assert record == get_jobs.Record()
+
+    assert record.id == 123
+    assert record.job_request_id == 4567
