93a14339e76ba3c58bcc0521b6c8307f93baa291
Alice Wong <alice.wong@thedatalab.org>
2025-07-03 17:17:05 +0100

    Add outcome column to `get_jobs` task
    - Define `outcome` as a categorical variable, with possible values being
    `cancelled by dependency`, `errored`, and `other`.
    
    - The `status` and `status_message` fields of a job are used to
    determine the `outcome` of the job.
    
    - Note that failures with `status_message` starting with
    "No outputs found matching patterns" count as errors even though outputs
    might have been successfully produced, as the project pipeline is not
    set up correctly.
    
    - "GitRepoNotReachableError"s also count as errors, as undoubtedly
    something has gone wrong.

--- tests/tasks/test_get_jobs.py
+++ tests/tasks/test_get_jobs.py
@@ -7,12 +7,22 @@ from tasks import get_jobs
 
 
 Row = collections.namedtuple(
-    "Row", ["id", "job_request_id", "created_at", "run_command"]
+    "Row",
+    ["id", "job_request_id", "created_at", "run_command", "status", "status_message"],
 )
 
 
 def test_transform():
-    rows = [Row(123, 4567, datetime.datetime(2025, 1, 1), "python:v2 foo.py")]
+    rows = [
+        Row(
+            123,
+            4567,
+            datetime.datetime(2025, 1, 1),
+            "python:v2 foo.py",
+            "succeeded",
+            "Completed successfully",
+        )
+    ]
     records = list(get_jobs.transform(rows))
     record = records[0]
 
@@ -20,6 +30,7 @@ def test_transform():
     assert record.job_request_id == 4567
     assert record.created_at == datetime.datetime(2025, 1, 1)
     assert record.stage == "analysis"
+    assert record.outcome == "other"
 
 
 @pytest.mark.parametrize(
@@ -33,3 +44,24 @@ def test_transform():
 )
 def test_get_stage(run_command, stage):
     assert get_jobs.get_stage(run_command) == stage
+
+
+@pytest.mark.parametrize(
+    "status, status_message, outcome",
+    [
+        ("failed", "Job exited with an error", "errored"),
+        ("failed", "Job exited with an error: ...", "errored"),
+        ("failed", "Internal error: this usually means...", "errored"),
+        ("failed", "No outputs found matching patterns: ...", "errored"),
+        ("failed", "GitRepoNotReachableError: Could not read from...", "errored"),
+        ("failed", "Not starting as dependency failed", "cancelled by dependency"),
+        ("failed", "Cancelled by user", "other"),
+        ("failed", "... Branch name must not contain slashes ...", "other"),
+        ("succeeded", "Completed successfully", "other"),
+        ("running", "Job executing on the backend", "other"),
+        ("pending", "Waiting on dependencies", "other"),
+        ("pending", "Waiting on available workers", "other"),
+    ],
+)
+def test_get_outcome(status, status_message, outcome):
+    assert get_jobs.get_outcome(status, status_message) == outcome
