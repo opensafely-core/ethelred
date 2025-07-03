b9adc653c76ed4a4b297b63ad4d65fffc8e9d38a
Alice Wong <alice.wong@thedatalab.org>
2025-07-03 15:48:06 +0100

    Add stage column to `get_jobs` task
    We define `stage` as a categorical column with two possible values:
    "database" and "analysis".
    The value of `stage` is obtained from the job's `run_command`,
    where ehrql jobs are classified as database jobs and
    all other jobs are classfied as analysis jobs.

--- tests/tasks/test_get_jobs.py
+++ tests/tasks/test_get_jobs.py
@@ -1,17 +1,35 @@
 import collections
 import datetime
 
+import pytest
+
 from tasks import get_jobs
 
 
-Row = collections.namedtuple("Row", ["id", "job_request_id", "created_at"])
+Row = collections.namedtuple(
+    "Row", ["id", "job_request_id", "created_at", "run_command"]
+)
 
 
 def test_transform():
-    rows = [Row(123, 4567, datetime.datetime(2025, 1, 1))]
+    rows = [Row(123, 4567, datetime.datetime(2025, 1, 1), "python:v2 foo.py")]
     records = list(get_jobs.transform(rows))
     record = records[0]
 
     assert record.id == 123
     assert record.job_request_id == 4567
     assert record.created_at == datetime.datetime(2025, 1, 1)
+    assert record.stage == "analysis"
+
+
+@pytest.mark.parametrize(
+    "run_command,stage",
+    [
+        ("ehrql:latest", "database"),
+        ("ehrql:v1", "database"),
+        ("python:v1", "analysis"),
+        ("r:latest", "analysis"),
+    ],
+)
+def test_get_stage(run_command, stage):
+    assert get_jobs.get_stage(run_command) == stage
