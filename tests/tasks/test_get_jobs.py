8c4a47c8403819f171f85f877c12c8d166d5e562
Alice Wong <alice.wong@thedatalab.org>
2025-07-03 14:53:46 +0100

    Extract job creation timestamp in `get_jobs` task
    - We extract and load the `created_at` timestamp
    for jobs so that we can analyse trends in time.
    
    - We may choose another timestamp to look at
    in the future, but we start with `created_at`.

--- tests/tasks/test_get_jobs.py
+++ tests/tasks/test_get_jobs.py
@@ -1,15 +1,17 @@
 import collections
+import datetime
 
 from tasks import get_jobs
 
 
-Row = collections.namedtuple("Row", ["id", "job_request_id"])
+Row = collections.namedtuple("Row", ["id", "job_request_id", "created_at"])
 
 
 def test_transform():
-    rows = [Row(123, 4567)]
+    rows = [Row(123, 4567, datetime.datetime(2025, 1, 1))]
     records = list(get_jobs.transform(rows))
     record = records[0]
 
     assert record.id == 123
     assert record.job_request_id == 4567
+    assert record.created_at == datetime.datetime(2025, 1, 1)
