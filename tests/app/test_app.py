590605ea4c81e3bad19b54e2a93432709eb31150
Alice Wong <alice.wong@thedatalab.org>
2025-07-07 14:27:35 +0100

    Show proportion of jobs cancelled by dependency
    - Add functionality for the app to read the CSV file of jobs.
    
    - Add calculation to aggregate the above into a single-column table of
    job requests, the column being the proportion of jobs cancelled by
    dependency over the total number of jobs that either errored or were
    cancelled by dependency. Job requests with no jobs that errored or
    cancelled by dependency are not of interest and hence excluded.
    
    - Show the distribution of the proportion of jobs that were cancelled
    by dependency in a histogram in the Streamlit app.

--- tests/app/test_app.py
+++ tests/app/test_app.py
@@ -1,11 +1,19 @@
+import pandas as pd
 from streamlit.testing.v1 import AppTest
+
+from app import app
 
 
 def test_app(tmp_path, monkeypatch):
-    tmp_f_path = tmp_path / "job_requests" / "job_requests.csv"
-    tmp_f_path.parent.mkdir()
-    tmp_f_path.write_text(
+    job_requests_path = tmp_path / "job_requests" / "job_requests.csv"
+    job_requests_path.parent.mkdir()
+    job_requests_path.write_text(
         "created_at,num_actions,num_jobs\n2025-01-01 00:00:00.0+00:00,1,1\n"
+    )
+    jobs_path = tmp_path / "jobs" / "jobs.csv"
+    jobs_path.parent.mkdir()
+    jobs_path.write_text(
+        "job_request_id,outcome\n123,errored\n123,cancelled by dependency\n"
     )
     monkeypatch.setenv("DATA_DIR", str(tmp_path))
 
@@ -13,3 +21,21 @@ def test_app(tmp_path, monkeypatch):
     app_test.run()
 
     assert not app_test.exception
+
+
+def test_calculate_proportions():
+    jobs = pd.DataFrame(
+        {
+            "job_request_id": [123, 123, 123, 456, 456, 789],
+            "outcome": [
+                "errored",
+                "cancelled by dependency",
+                "other",
+                "errored",
+                "other",
+                "other",
+            ],
+        }
+    )
+    df = app.calculate_proportions(jobs)
+    assert df.to_dict() == {"proportion": {123: 1 / 2, 456: 0.0}}
