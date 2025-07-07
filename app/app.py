3481161293a547aac49950fa2c8be270d83038ea
Alice Wong <alice.wong@thedatalab.org>
2025-07-07 14:27:35 +0100

    Show proportion of jobs cancelled by dependency
    - Add functionality for the app to read the CSV file of jobs.
    
    - Add calculation to aggregate the above into a single-column table of
    job requests, the column being the proportion of jobs cancelled by
    dependency over the total number of jobs that either errored or were
    cancelled by dependency. Job requests with no jobs that errored or
    cancelled by dependency are not of interest and hence excluded.
    
    - Show the distribution of the proportion of jobs that were cancelled
    by dependency in a histogram in the Streamlit app.

--- app/app.py
+++ app/app.py
@@ -15,6 +15,36 @@ def get_job_requests(f_path):
     return job_requests
 
 
+def get_jobs(f_path):
+    jobs = pandas.read_csv(f_path)
+    return jobs
+
+
+def calculate_proportions(jobs):
+    """
+    Returns a one row per job request DataFrame with a single column "proportion"
+    that contains the proportion of jobs cancelled by dependency out of all
+    jobs that either errored or were cancelled by dependency.
+
+    Job requests with no jobs that errored or were cancelled by dependency
+    are filtered out.
+    """
+    job_requests = (
+        jobs.groupby(["job_request_id", "outcome"])
+        .size()
+        .unstack(fill_value=0)
+        .add_prefix("num_")
+    )
+    job_requests["denominator"] = (
+        job_requests["num_errored"] + job_requests["num_cancelled by dependency"]
+    )
+    job_requests = job_requests.loc[job_requests["denominator"] > 0]
+    job_requests["proportion"] = (
+        job_requests["num_cancelled by dependency"] / job_requests["denominator"]
+    )
+    return job_requests.loc[:, "proportion"].to_frame()
+
+
 def get_histogram(job_requests, column_name):
     return (
         altair.Chart(job_requests)
@@ -30,6 +60,7 @@ def get_scatter_plot(job_requests, column_names):
 
 def main():
     job_requests = get_job_requests(DATA_DIR / "job_requests" / "job_requests.csv")
+    jobs = get_jobs(DATA_DIR / "jobs" / "jobs.csv")
 
     num_actions_histogram = get_histogram(job_requests, "num_actions")
     streamlit.write(num_actions_histogram)
@@ -39,6 +70,9 @@ def main():
 
     measure_histogram = get_histogram(job_requests, "measure")
     streamlit.write(measure_histogram)
+
+    proportion_histogram = get_histogram(calculate_proportions(jobs), "proportion")
+    streamlit.write(proportion_histogram)
 
     scatter_plot = get_scatter_plot(job_requests, ("num_actions", "measure"))
     streamlit.write(scatter_plot)
