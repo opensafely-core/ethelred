577f7311dc5310a9f1ebd186cb121042e5abd38d
Alice Wong <alice.wong@thedatalab.org>
2025-07-08 13:54:05 +0100

    Test extraction in `tasks.get_jobs`
    - Previously the extraction step in `tasks.get_jobs` was hard to test
    due to the lack of a Job Server DB for testing.
    - We now have a fake DB, so remove the `pragma: no cover` and test the
    extraction step. As part of this process, the extraction step is now
    split into two parts: `get_query` and `extract`.
    
    Closes #12

--- tests/tasks/test_get_jobs.py
+++ tests/tasks/test_get_jobs.py
@@ -2,6 +2,7 @@ import collections
 import datetime
 
 import pytest
+import sqlalchemy
 
 from tasks import get_jobs
 
@@ -10,6 +11,74 @@ Row = collections.namedtuple(
     "Row",
     ["id", "job_request_id", "created_at", "run_command", "status", "status_message"],
 )
+
+
+@pytest.fixture
+def make_job():
+    def _make_job(id_, job_request_id, created_at):
+        return {
+            "id": id_,
+            "job_request_id": job_request_id,
+            "created_at": created_at,
+            "action": "do_something",
+            "run_command": "command:version some_script.py",
+            "status": "some status",
+            "status_code": "000",
+            "status_message": "some status message",
+        }
+
+    return _make_job
+
+
+def test_get_query(jobserver_engine, jobserver_metadata, make_job, make_jobrequest):
+    # arrange
+    job_request_table = jobserver_metadata.tables["jobserver_jobrequest"]
+    # before index date
+    jobrequest_1 = make_jobrequest(
+        id_=1,
+        created_at=datetime.datetime(2024, 1, 1),
+    )
+    # on or after index date
+    jobrequest_2 = make_jobrequest(
+        id_=2,
+        created_at=datetime.datetime(2025, 1, 1),
+    )
+    insert_into_jobrequest_table = sqlalchemy.insert(job_request_table).values(
+        [jobrequest_1, jobrequest_2]
+    )
+
+    job_table = jobserver_metadata.tables["jobserver_job"]
+    job_1 = make_job(
+        id_=123,
+        job_request_id=1,
+        created_at=datetime.datetime(2024, 1, 1),
+    )
+    job_2 = make_job(
+        id_=456,
+        job_request_id=2,
+        created_at=datetime.datetime(2025, 1, 2),
+    )
+    insert_into_job_table = sqlalchemy.insert(job_table).values([job_1, job_2])
+
+    with jobserver_engine.connect() as conn:
+        conn.execute(insert_into_jobrequest_table)
+        conn.execute(insert_into_job_table)
+        conn.commit()
+
+    # act
+    query = get_jobs.get_query(jobserver_metadata)
+    rows = get_jobs.extract(jobserver_engine, query)
+    rows = list(rows)
+
+    # assert
+    assert len(rows) == 1
+    row = rows[0]
+    assert row.id == 456
+    assert row.job_request_id == 2
+    assert row.created_at == datetime.datetime(2025, 1, 2, tzinfo=datetime.timezone.utc)
+    assert row.run_command == "command:version some_script.py"
+    assert row.status == "some status"
+    assert row.status_message == "some status message"
 
 
 def test_transform():
