5947afc19fb2ee5c8f793f1c71bedb0863fbad87
Iain Dillingham <iain@dillingham.me.uk>
2025-07-09 20:59:59 +0100

    Test `tasks.get_job_requests.extract`
    This adds a test and removes the pragma. The test has a substantial
    arrange step by necessity and design. By necessity, because there's a
    lot to arrange. By design, because we'd like to keep the fixtures
    separate from other tests, even though several are very similar to the
    fixtures in other tests. Doing so makes it much easier to update this
    test, without affecting other tests.
    
    Co-authored-by: Alice Wong <146962159+alarthast@users.noreply.github.com>

--- tests/tasks/test_get_job_requests.py
+++ tests/tasks/test_get_job_requests.py
@@ -1,12 +1,98 @@
 import collections
 import datetime
 
+import sqlalchemy
+
 from tasks import get_job_requests, io
 
 
 Row = collections.namedtuple(
     "Row", ["url", "sha", "created_at", "num_jobs", "username"]
 )
+
+
+def test_extract(jobserver_engine, jobserver_metadata):
+    # arrange
+    a_foreign_key = 99
+
+    repo_table = jobserver_metadata.tables["jobserver_repo"]
+    repo_id = 1
+    insert_into_repo_table = sqlalchemy.insert(repo_table).values(
+        id=repo_id,
+        url="https://github.com/opensafely/my-repo",
+        has_github_outputs=False,
+    )
+
+    workspace_table = jobserver_metadata.tables["jobserver_workspace"]
+    workspace_id = 1
+    insert_into_workspace_table = sqlalchemy.insert(workspace_table).values(
+        id=workspace_id,
+        created_by_id=a_foreign_key,
+        project_id=a_foreign_key,
+        uses_new_release_flow=True,
+        repo_id=repo_id,
+        signed_off_by_id=a_foreign_key,
+        purpose="a purpose",
+        updated_at=datetime.datetime(2025, 1, 1),
+        updated_by_id=a_foreign_key,
+    )
+
+    user_table = jobserver_metadata.tables["jobserver_user"]
+    user_id = 1
+    insert_into_user_table = sqlalchemy.insert(user_table).values(
+        id=user_id, username="a_user", fullname="a user", roles=[]
+    )
+
+    jobrequest_table = jobserver_metadata.tables["jobserver_jobrequest"]
+    template_jobrequest = {
+        "id": None,  # replace me
+        "sha": None,  # replace me
+        "created_at": None,  # replace me
+        "backend_id": a_foreign_key,
+        "created_by_id": user_id,
+        "workspace_id": workspace_id,
+        "requested_actions": ["a1"],
+        "project_definition": "actions: {a1: {}, a2: {}}",
+        "codelists_ok": True,
+    }
+    # before index date
+    jobrequest_1 = template_jobrequest | {
+        "id": 1,
+        "sha": "1111111",
+        "created_at": datetime.datetime(2024, 1, 1),
+    }
+    # on or after index date
+    jobrequest_2 = template_jobrequest | {
+        "id": 2,
+        "sha": "2222222",
+        "created_at": datetime.datetime(2025, 1, 1),
+    }
+    insert_into_jobrequest_table = sqlalchemy.insert(jobrequest_table).values(
+        [jobrequest_1, jobrequest_2]
+    )
+
+    job_table = jobserver_metadata.tables["jobserver_job"]
+    job_1 = {"id": 1, "job_request_id": 1, "run_command": "my-command:v1"}
+    job_2 = {"id": 2, "job_request_id": 2, "run_command": "my-command:v1"}
+    insert_into_job_table = sqlalchemy.insert(job_table).values([job_1, job_2])
+
+    with jobserver_engine.connect() as conn:
+        conn.execute(insert_into_repo_table)
+        conn.execute(insert_into_workspace_table)
+        conn.execute(insert_into_user_table)
+        conn.execute(insert_into_jobrequest_table)
+        conn.execute(insert_into_job_table)
+        conn.commit()
+
+    # act
+    rows = list(get_job_requests.extract(jobserver_engine, jobserver_metadata))
+
+    # assert
+    assert len(rows) == 1
+    row = rows[0]
+    assert row._fields == ("url", "sha", "created_at", "num_jobs", "username")
+    assert row.sha == "2222222"
+    assert row.num_jobs == 1
 
 
 def test_load_project_definition(tmp_path):
