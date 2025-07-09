cfa12dec4d86d69603c9a74f7987d5d867b3514a
Iain Dillingham <iain@dillingham.me.uk>
2025-07-09 12:17:02 +0100

    Revert "Use enums for job stages and outcomes"
    This reverts commit 3d447c6f686549a6d317cf6950bf5e98285824cb.

--- tests/tasks/test_get_jobs.py
+++ tests/tasks/test_get_jobs.py
@@ -29,51 +29,39 @@ def test_transform():
     assert record.id == 123
     assert record.job_request_id == 4567
     assert record.created_at == datetime.datetime(2025, 1, 1)
-    assert record.stage == get_jobs.Stage.ANALYSIS.value
-    assert record.outcome == get_jobs.Outcome.OTHER.value
+    assert record.stage == "analysis"
+    assert record.outcome == "other"
 
 
 @pytest.mark.parametrize(
     "run_command,stage",
     [
-        ("ehrql:latest", get_jobs.Stage.DATABASE),
-        ("ehrql:v1", get_jobs.Stage.DATABASE),
-        ("python:v1", get_jobs.Stage.ANALYSIS),
-        ("r:latest", get_jobs.Stage.ANALYSIS),
+        ("ehrql:latest", "database"),
+        ("ehrql:v1", "database"),
+        ("python:v1", "analysis"),
+        ("r:latest", "analysis"),
     ],
 )
 def test_get_stage(run_command, stage):
-    assert get_jobs.get_stage(run_command) == stage.value
+    assert get_jobs.get_stage(run_command) == stage
 
 
 @pytest.mark.parametrize(
     "status, status_message, outcome",
     [
-        ("failed", "Job exited with an error", get_jobs.Outcome.ERRORED),
-        ("failed", "Job exited with an error: ...", get_jobs.Outcome.ERRORED),
-        ("failed", "Internal error: this usually means...", get_jobs.Outcome.ERRORED),
-        ("failed", "No outputs found matching patterns: ...", get_jobs.Outcome.ERRORED),
-        (
-            "failed",
-            "GitRepoNotReachableError: Could not read ...",
-            get_jobs.Outcome.ERRORED,
-        ),
-        (
-            "failed",
-            "Not starting as dependency failed",
-            get_jobs.Outcome.CANCELLED_BY_DEPENDENCY,
-        ),
-        ("failed", "Cancelled by user", get_jobs.Outcome.OTHER),
-        (
-            "failed",
-            "GithubValidationError: Branch name must not contain slashes ...",
-            get_jobs.Outcome.OTHER,
-        ),
-        ("succeeded", "Completed successfully", get_jobs.Outcome.OTHER),
-        ("running", "Job executing on the backend", get_jobs.Outcome.OTHER),
-        ("pending", "Waiting on dependencies", get_jobs.Outcome.OTHER),
-        ("pending", "Waiting on available workers", get_jobs.Outcome.OTHER),
+        ("failed", "Job exited with an error", "errored"),
+        ("failed", "Job exited with an error: ...", "errored"),
+        ("failed", "Internal error: this usually means...", "errored"),
+        ("failed", "No outputs found matching patterns: ...", "errored"),
+        ("failed", "GitRepoNotReachableError: Could not read from...", "errored"),
+        ("failed", "Not starting as dependency failed", "cancelled by dependency"),
+        ("failed", "Cancelled by user", "other"),
+        ("failed", "... Branch name must not contain slashes ...", "other"),
+        ("succeeded", "Completed successfully", "other"),
+        ("running", "Job executing on the backend", "other"),
+        ("pending", "Waiting on dependencies", "other"),
+        ("pending", "Waiting on available workers", "other"),
     ],
 )
 def test_get_outcome(status, status_message, outcome):
-    assert get_jobs.get_outcome(status, status_message) == outcome.value
+    assert get_jobs.get_outcome(status, status_message) == outcome
