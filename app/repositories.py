89dc85fbe187d8a805528992bd8115da9df075ac
Iain Dillingham <iain@dillingham.me.uk>
2025-07-21 09:25:11 +0100

    Cast `root_dir` to `pathlib.Path`
    This makes it easier to work with a `Repository` interactively, as one
    can pass a string. Passing a `pathlib.Path` to a `pathlib.Path` also
    returns a `pathlib.Path`, so this is backwards-compatible.

--- app/repositories.py
+++ app/repositories.py
@@ -1,8 +1,11 @@
+import pathlib
+
 import pandas
 
 
 class Repository:
     def __init__(self, root_dir):
+        root_dir = pathlib.Path(root_dir)
         self.job_requests_csv = root_dir / "job_requests" / "job_requests.csv"
         self.jobs_csv = root_dir / "jobs" / "jobs.csv"
 
