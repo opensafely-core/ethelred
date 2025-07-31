7515fbbcf5098bef56ba4adab4ad2af65e1e1e7b
Iain Dillingham <iain@dillingham.me.uk>
2025-07-31 08:10:07 +0100

    Merge tests
    We merge `test_pickle` and `test_json` into `test_one_record_per_file`.
    To clarify the nature of the tests, we also rename `test_csv` to
    `test_many_records_per_file`.
    
    As an aside, for consistency with `pathlib.Path.suffix` we ensure that
    each suffix has a dot (e.g. ".pickle" rather than simply "pickle").

--- tests/tasks/test_io.py
+++ tests/tasks/test_io.py
@@ -5,25 +5,19 @@ import pytest
 from tasks import io
 
 
-def test_pickle(tmp_path):
-    f_path = tmp_path / "subdir" / "obj.pickle"
+@pytest.mark.parametrize("suffix", [".pickle", ".json"])
+def test_one_record_per_file(tmp_path, suffix):
+    f_path = tmp_path / "subdir" / f"obj{suffix}"
     io.write({"my_key": "my_value"}, f_path)
     obj = io.read(f_path)
     assert obj == {"my_key": "my_value"}
 
 
-def test_csv(tmp_path):
+def test_many_records_per_file(tmp_path):
     Record = collections.namedtuple("Record", ["name"])
     f_path = tmp_path / "subdir" / "records.csv"
     io.write([Record("name_a"), Record("name_b")], f_path)
     assert f_path.read_text() == "name\n" + "name_a\n" + "name_b\n"
-
-
-def test_json(tmp_path):
-    f_path = tmp_path / "subdir" / "obj.json"
-    io.write({"my_key": "my_value"}, f_path)
-    obj = io.read(f_path)
-    assert obj == {"my_key": "my_value"}
 
 
 def test_write_unsupported_file_type(tmp_path):
