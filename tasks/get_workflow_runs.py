ac22f9e2cfeddc14ef148255f350c03b1d00f4f9
Alice Wong <alice.wong@thedatalab.org>
2025-08-14 16:04:05 +0100

    Rewrite `get_workflow_runs` task
    The timestamp format has changed to a more readable format but otherwise
    does the same thing as before with some efficiency improvements.
    
    See #78.

--- tasks/get_workflow_runs.py
+++ tasks/get_workflow_runs.py
@@ -1,5 +1,7 @@
 import collections
-import functools
+import datetime
+import itertools
+import json
 import os
 import time
 
@@ -8,10 +10,9 @@ import requests
 from . import DATA_DIR, io
 
 
-REPOS_URL = "https://api.github.com/orgs/opensafely/repos"
-WORKFLOW_RUNS_URL_TEMPLATE = (
-    "https://api.github.com/repos/opensafely/{repo}/actions/runs"
-)
+GITHUB_ORG = "opensafely"
+
+File = collections.namedtuple("File", ["filepath", "text"])
 Record = collections.namedtuple(
     "Record",
     [
@@ -28,66 +29,48 @@ Record = collections.namedtuple(
 )
 
 
-def write_log(message):  # pragma: no cover
-    # Placeholder: printing log messages to console for now.
-    print(message)
-
-
 class GitHubAPISession(requests.Session):
     def __init__(self, token=None):
         super().__init__()
         token = os.environ["GITHUB_WORKFLOW_RUNS_TOKEN"]
         self.headers.update({"Authorization": f"Bearer {token}"})
+        self.params.update({"per_page": 100, "format": "json"})
 
 
-def retry(log, max_retries=3, backoff_seconds=0.5):
-    def decorator(get):
-        @functools.wraps(get)
-        def wrapper(url, **kwargs):
-            retry_count = 0
-            while True:
-                try:
-                    response = get(url, **kwargs)
-                    response.raise_for_status()
+class SessionWithRetry:
+    def __init__(
+        self, session, max_retries=3, base_delay_seconds=0.5, sleep_function=time.sleep
+    ):
+        self.session = session
+        self.max_retries = max_retries
+        self.base_delay_seconds = base_delay_seconds
+        self.sleep = sleep_function
+
+    def get(self, url):
+        retry_count = 0
+        while True:
+            try:
+                response = self.session.get(url)
+                response.raise_for_status()
+                return response
+            except Exception as error:
+                print(f"Error fetching {url}: {error}")
+                if retry_count < self.max_retries:
+                    delay_seconds = self.base_delay_seconds * (2**retry_count)
+                    print(
+                        f"Retrying in {delay_seconds} seconds (retry attempt {retry_count + 1})..."
+                    )
+                    retry_count += 1
+                    self.sleep(delay_seconds)
+                else:
+                    print(f"Maximum retries reached ({self.max_retries}).")
                     return response
-                except requests.RequestException as error:
-                    if retry_count < max_retries:
-                        seconds = backoff_seconds * (2**retry_count)
-                        log(
-                            f"Error fetching {url}: {error}\n"
-                            f"Retrying in {seconds} seconds (retry attempt {retry_count + 1}) ..."
-                        )
-                        time.sleep(seconds)
-                        retry_count += 1
-                    else:
-                        log(
-                            f"Error fetching {url}: {error}\n"
-                            f"Maximum retries reached ({max_retries})."
-                        )
-                        return response
-
-        return wrapper
-
-    return decorator
 
 
-def write_page(page, output_dir, page_number):
-    filename = output_dir / "pages" / f"page_{page_number}.json"
-    io.write(page, filename)
-
-
-def write_run(run, output_dir):
-    filename = output_dir / "runs" / f"{run['id']}.json"
-    io.write(run, filename)
-
-
-def get_all_pages_with_retry(get_function, first_url, **kwargs):
-    url = first_url
+def get_pages(session, first_page_url):
+    url = first_page_url
     while True:
-        response = retry(write_log)(get_function)(url, **kwargs)
-        if response.status_code != 200:
-            write_log("Skipping.")
-            break  # give up as we have already retried
+        response = session.get(url)
         yield response
         if next_link := response.links.get("next"):
             url = next_link["url"]
@@ -95,79 +78,95 @@ def get_all_pages_with_retry(get_function, first_url, **kwargs):
             break
 
 
-def get_repos_pages(session):
-    yield from (
-        response.json()
-        for response in get_all_pages_with_retry(
-            session.get, REPOS_URL, params={"format": "json", "per_page": 100}
-        )
-    )
+def _extract_repo_names_from_pages(repos_pages):
+    decoded_pages = (page.json() for page in repos_pages)
+    return (repo["name"] for page in decoded_pages for repo in page)
 
 
-def get_workflow_runs_pages(repo, session):
-    first_page_url = WORKFLOW_RUNS_URL_TEMPLATE.format(repo=repo)
-    yield from (
-        response.json()["workflow_runs"]
-        for response in get_all_pages_with_retry(
-            session.get, first_page_url, params={"format": "json", "per_page": 100}
-        )
-    )
+def get_repo_names(session):
+    url = f"https://api.github.com/orgs/{GITHUB_ORG}/repos"
+    pages_1, pages_2 = itertools.tee(get_pages(session, url))
+    return pages_1, _extract_repo_names_from_pages(pages_2)
 
 
-def get_latest_run_filepaths(repo_dir):
-    files = {}
-    timestamps = [int(d.name) for d in repo_dir.iterdir() if d.is_dir()]
-    for timestamp in sorted(timestamps, reverse=True):
-        if not (runs_dir := repo_dir / str(timestamp) / "runs").exists():
+def _extract_workflow_runs_from_pages(workflow_runs_pages):
+    decoded_pages = (page.json() for page in workflow_runs_pages)
+    return (run for page in decoded_pages for run in page["workflow_runs"])
+
+
+def get_repo_workflow_runs(repo_name, session):
+    url = f"https://api.github.com/repos/{GITHUB_ORG}/{repo_name}/actions/runs"
+    pages_1, pages_2 = itertools.tee(get_pages(session, url))
+    return pages_1, _extract_workflow_runs_from_pages(pages_2)
+
+
+def get_page_files(pages, output_dir):
+    for page_number, page in enumerate(pages, start=1):
+        yield File(output_dir / "pages" / f"{page_number}.json", page.text)
+
+
+def get_run_files(workflow_runs, output_dir):
+    for run in workflow_runs:
+        yield File(output_dir / "runs" / f"{run['id']}.json", json.dumps(run))
+
+
+def extract(session, output_dir, datetime_, write_function):
+    timestamp = datetime_.strftime("%Y%m%d-%H%M%SZ")
+    repo_pages, repo_names = get_repo_names(session)
+    repo_files = get_page_files(repo_pages, output_dir / "repos" / timestamp)
+
+    file_iterables = [repo_files]
+    for repo_name in repo_names:
+        run_pages, runs = get_repo_workflow_runs(repo_name, session)
+        page_files = get_page_files(run_pages, output_dir / repo_name / timestamp)
+        run_files = get_run_files(runs, output_dir / repo_name / timestamp)
+        file_iterables.extend([page_files, run_files])
+
+    for file in itertools.chain(*file_iterables):
+        write_function(file.text, file.filepath)
+
+
+def get_names_of_extracted_repos(workflows_dir):
+    # Being deterministic is more important than saving memory for a couple of strings
+    return sorted(repo.name for repo in workflows_dir.iterdir() if repo.is_dir())
+
+
+def load_latest_workflow_runs(repo_dir):
+    filepaths = sorted(repo_dir.glob("*/runs/*.json"), reverse=True)
+    seen = set()
+    for filepath in filepaths:
+        if filepath.name in seen:
             continue
-        dir_files = {
-            file_path.name: file_path
-            for file_path in runs_dir.iterdir()
-            if file_path.suffix == ".json"
-        }
-        files = dir_files | files  # keep the most recent files
-    return [files[filename] for filename in sorted(files.keys())]
+        seen.add(filepath.name)
+        with filepath.open() as f:
+            yield json.load(f)
 
 
-def get_record(run):
-    return Record(
-        id=run["id"],
-        repo=run["repository"]["name"],
-        name=run["name"],
-        head_sha=run["head_sha"],
-        status=run["status"],
-        conclusion=run["conclusion"],
-        created_at=run["created_at"],
-        updated_at=run["updated_at"],
-        run_started_at=run["run_started_at"],
+def get_records(workflows_dir):
+    repos = get_names_of_extracted_repos(workflows_dir)
+    workflow_runs = (
+        run for repo in repos for run in load_latest_workflow_runs(workflows_dir / repo)
     )
+    for run in workflow_runs:
+        yield Record(
+            id=run["id"],
+            repo=run["repository"]["name"],
+            name=run["name"],
+            head_sha=run["head_sha"],
+            status=run["status"],
+            conclusion=run["conclusion"],
+            created_at=run["created_at"],
+            updated_at=run["updated_at"],
+            run_started_at=run["run_started_at"],
+        )
 
 
-def main(session, workflows_dir):
-    # Use the same timestamp to name all directories in this extraction
-    timestamp = int(time.time())
-    repos_pages = get_repos_pages(session)
-    repo_names = []
-    for page_number, repos_page in enumerate(repos_pages, start=1):
-        write_page(repos_page, workflows_dir / "repos" / str(timestamp), page_number)
-        for repo in (repo["name"] for repo in repos_page):
-            runs_pages = get_workflow_runs_pages(repo, session)
-            for page_number, runs_page in enumerate(runs_pages, start=1):
-                write_page(
-                    runs_page, workflows_dir / repo / str(timestamp), page_number
-                )
-                for run in runs_page:
-                    write_run(run, workflows_dir / repo / str(timestamp))
-            repo_names.append(repo)
-
-    filepaths = [
-        filepath
-        for filepaths in [
-            get_latest_run_filepaths(workflows_dir / repo) for repo in repo_names
-        ]
-        for filepath in filepaths
-    ]
-    records = (get_record(io.read(filepath)) for filepath in filepaths)
+def main(session, workflows_dir, now_function=datetime.datetime.now):
+    # Extract and write data to disk
+    extract(SessionWithRetry(session), workflows_dir, now_function(), io.write)
+    # Get latest workflow runs from disk (may include past extractions)
+    records = get_records(workflows_dir)
+    # Load
     io.write(records, workflows_dir / "workflow_runs.csv")
 
 
