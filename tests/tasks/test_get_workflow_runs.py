1e5473e7aeab0456c6c5aae0de1f81ec17cfca51
Alice Wong <alice.wong@thedatalab.org>
2025-08-28 15:39:31 +0100

    Revert "Use `updated_at` timestamp of data for filename"
    The commit message needs rewriting.
    
    This reverts commit 5d07a2bd8ab59ef59b972e36a1362e992f5f9539.

--- tests/tasks/test_get_workflow_runs.py
+++ tests/tasks/test_get_workflow_runs.py
@@ -1,3 +1,5 @@
+import datetime
+import json
 import pathlib
 import types
 
@@ -108,50 +110,24 @@ def test_extract(tmpdir, monkeypatch):
     responses.add(
         responses.GET,
         "https://api.github.com/orgs/test-org/repos",
-        json=[
-            {
-                "name": "repo_1",
-                "updated_at": "2025-01-01T00:00:00Z",
-            }
-        ],
+        json=[{"name": "repo_1"}],
         headers={"Link": '<https://repos.page2>; rel="next"'},
     )
     responses.add(
         responses.GET,
         "https://repos.page2",
-        json=[
-            {
-                "name": "repo_2",
-                "updated_at": "2025-01-02T00:00:00Z",
-            }
-        ],
+        json=[{"name": "repo_2"}],
     )
     responses.add(
         responses.GET,
         "https://api.github.com/repos/test-org/repo_1/actions/runs",
-        json={
-            "total_count": 2,
-            "workflow_runs": [
-                {
-                    "id": 1,
-                    "updated_at": "2025-01-03T00:00:00Z",
-                }
-            ],
-        },
+        json={"total_count": 2, "workflow_runs": [{"id": 1}]},
         headers={"Link": '<https://repo_1/runs.page2>; rel="next"'},
     )
     responses.add(
         responses.GET,
         "https://repo_1/runs.page2",
-        json={
-            "total_count": 2,
-            "workflow_runs": [
-                {
-                    "id": 2,
-                    "updated_at": "2025-01-04T00:00:00Z",
-                }
-            ],
-        },
+        json={"total_count": 2, "workflow_runs": [{"id": 2}]},
     )
     responses.add(
         responses.GET,
@@ -160,46 +136,43 @@ def test_extract(tmpdir, monkeypatch):
     )
 
     output_dir = pathlib.Path(tmpdir)
-    get_workflow_runs.extract("test-org", output_dir)
+    get_workflow_runs.extract("test-org", output_dir, datetime.datetime(2025, 1, 1))
 
-    assert io.read(output_dir / "repos" / "repo_1" / "20250101-000000.json") == {
-        "name": "repo_1",
-        "updated_at": "2025-01-01T00:00:00Z",
+    assert io.read(output_dir / "repos" / "20250101-000000" / "repo_1.json") == {
+        "name": "repo_1"
     }
-    assert io.read(output_dir / "repos" / "repo_2" / "20250102-000000.json") == {
-        "name": "repo_2",
-        "updated_at": "2025-01-02T00:00:00Z",
+    assert io.read(output_dir / "repos" / "20250101-000000" / "repo_2.json") == {
+        "name": "repo_2"
     }
-    assert io.read(output_dir / "runs" / "repo_1" / "1" / "20250103-000000.json") == {
-        "id": 1,
-        "updated_at": "2025-01-03T00:00:00Z",
+    assert io.read(output_dir / "runs" / "repo_1" / "20250101-000000" / "1.json") == {
+        "id": 1
     }
-    assert io.read(output_dir / "runs" / "repo_1" / "2" / "20250104-000000.json") == {
-        "id": 2,
-        "updated_at": "2025-01-04T00:00:00Z",
+    assert io.read(output_dir / "runs" / "repo_1" / "20250101-000000" / "2.json") == {
+        "id": 2
     }
 
 
-def test_filter_workflow_run_filepaths(tmpdir):
-    parent = pathlib.Path(tmpdir) / "repo_1"
+def test_filter_workflow_run_filepaths():
+    older_dir = pathlib.Path("repo_1") / "20250101-000000"
+    newer_dir = pathlib.Path("repo_1") / "20250102-000000"
+
     filepaths = [
-        parent / "1" / "20250101-000000.json",
-        parent / "2" / "20250101-000000.json",
-        parent / "2" / "20250102-000000.json",
-        parent / "3" / "20250102-000000.json",
+        older_dir / "1.json",
+        older_dir / "2.json",
+        newer_dir / "2.json",
+        newer_dir / "3.json",
     ]
 
     filtered = get_workflow_runs.filter_workflow_run_filepaths(filepaths)
 
     assert list(filtered) == [
-        parent / "3" / "20250102-000000.json",
-        parent / "2" / "20250102-000000.json",
-        parent / "1" / "20250101-000000.json",
+        newer_dir / "3.json",
+        newer_dir / "2.json",
+        older_dir / "1.json",
     ]
 
 
 def test_get_records(tmpdir):
-    # Timestamp handling is tested elsewhere so use same updated_at for all runs
     template = {
         "id": None,  # Placeholder
         "name": "My Workflow",
@@ -213,13 +186,19 @@ def test_get_records(tmpdir):
             "name": None  # Placeholder
         },
     }
+    # Timestamp handling is tested elsewhere so only one timestamp per repo here
     runs_dir = pathlib.Path(tmpdir)
+    repo_1_dir = runs_dir / "repo_1" / "20250101-000000"
+    repo_2_dir = runs_dir / "repo_2" / "20250101-000000"
+    repo_1_dir.mkdir(parents=True)
+    repo_2_dir.mkdir(parents=True)
+
     repo_1_run_1 = template | {"id": 1, "repository": {"name": "repo_1"}}
-    io.write(repo_1_run_1, runs_dir / "repo_1" / "1" / "20250101-000000.json")
+    (repo_1_dir / "1.json").write_text(json.dumps(repo_1_run_1))
     repo_1_run_2 = template | {"id": 2, "repository": {"name": "repo_1"}}
-    io.write(repo_1_run_2, runs_dir / "repo_1" / "2" / "20250101-000000.json")
+    (repo_1_dir / "2.json").write_text(json.dumps(repo_1_run_2))
     repo_2_run_3 = template | {"id": 3, "repository": {"name": "repo_2"}}
-    io.write(repo_2_run_3, runs_dir / "repo_2" / "3" / "20250101-000000.json")
+    (repo_2_dir / "3.json").write_text(json.dumps(repo_2_run_3))
 
     records = get_workflow_runs.get_records(runs_dir)
     record_1, record_2, record_3 = sorted(records, key=lambda r: r.id)
@@ -242,22 +221,25 @@ def test_main(tmpdir, monkeypatch):
     # Run through pipeline for a single workflow run
     workflows_dir = pathlib.Path(tmpdir)
 
+    def mock_now(timezone):
+        return datetime.datetime(2025, 1, 1, tzinfo=timezone)
+
     run = {
         "id": 1,
         "name": "My Workflow",
         "head_sha": 12345678,
         "status": "pending",
         "conclusion": None,
-        "created_at": "2025-01-02T00:00:00Z",
-        "updated_at": "2025-01-04T00:00:00Z",
-        "run_started_at": "2025-01-03T00:00:00Z",
+        "created_at": "2025-01-01T00:00:00Z",
+        "updated_at": "2025-01-01T00:00:00Z",
+        "run_started_at": "2025-01-01T00:00:00Z",
         "repository": {"name": "test_repo"},
     }
 
     responses.add(
         responses.GET,
         "https://api.github.com/orgs/test-org/repos",
-        json=[{"name": "test_repo", "updated_at": "2025-01-01T00:00:00Z"}],
+        json=[{"name": "test_repo"}],
     )
     responses.add(
         responses.GET,
@@ -265,20 +247,19 @@ def test_main(tmpdir, monkeypatch):
         json={"total_count": 1, "workflow_runs": [run]},
     )
 
-    get_workflow_runs.main("test-org", workflows_dir)
+    get_workflow_runs.main("test-org", workflows_dir, now_function=mock_now)
 
     with open(workflows_dir / "workflow_runs.csv") as f:
         csv_file = f.read()
 
-    assert io.read(workflows_dir / "repos" / "test_repo" / "20250101-000000.json") == {
-        "name": "test_repo",
-        "updated_at": "2025-01-01T00:00:00Z",
+    assert io.read(workflows_dir / "repos" / "20250101-000000" / "test_repo.json") == {
+        "name": "test_repo"
     }
     assert (
-        io.read(workflows_dir / "runs" / "test_repo" / "1" / "20250104-000000.json")
+        io.read(workflows_dir / "runs" / "test_repo" / "20250101-000000" / "1.json")
         == run
     )
     assert csv_file == (
         "id,repo,name,head_sha,status,conclusion,created_at,updated_at,run_started_at\n"
-        "1,test_repo,My Workflow,12345678,pending,,2025-01-02T00:00:00Z,2025-01-04T00:00:00Z,2025-01-03T00:00:00Z\n"
+        "1,test_repo,My Workflow,12345678,pending,,2025-01-01T00:00:00Z,2025-01-01T00:00:00Z,2025-01-01T00:00:00Z\n"
     )
