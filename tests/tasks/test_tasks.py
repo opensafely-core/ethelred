00e6ad39b65063afd0bbae6b68cfb0dc1ffa33c8
Iain Dillingham <iain@dillingham.me.uk>
2025-10-13 11:48:03 +0100

    Test `<task>.main` doesn't accept parameters
    Task modules have an implicit interface (14557ac). To add a task runner
    (#134), it's convenient to extend this interface such that the `main`
    function doesn't accept parameters. Let's add a test to ensure that any
    modules we add to `tasks.tasks` in the future implement this interface.
    Because an existing module doesn't yet implement it, we mark the test as
    `xfail`.

--- tests/tasks/test_tasks.py
+++ tests/tasks/test_tasks.py
@@ -1,12 +1,19 @@
+import inspect
 import pkgutil
+
+import pytest
 
 import tasks.tasks
 
 
+@pytest.mark.xfail
 def test_task_modules_have_required_attributes():
     for _, modname, _ in pkgutil.iter_modules(tasks.tasks.__path__):
         dotted_modname = f"{tasks.tasks.__name__}.{modname}"
         mod = pkgutil.resolve_name(dotted_modname)
         assert hasattr(mod, "main"), (
             f"`{dotted_modname}` does not contain a `main` attribute"
+        )
+        assert len(inspect.signature(mod.main).parameters) == 0, (
+            f"`{dotted_modname}.main` accepts parameters"
         )
