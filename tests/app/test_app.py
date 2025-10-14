a829dac5f5345a78bde446fbe7b4baafc3a1a991
Iain Dillingham <iain@dillingham.me.uk>
2025-10-14 11:12:05 +0100

    Pare back repositories

--- tests/app/test_app.py
+++ tests/app/test_app.py
@@ -1,32 +1,9 @@
-import datetime
-
-import pandas
 from streamlit.testing.v1 import AppTest
 
 from app import app, repositories
 
 
-class FakeRepository(repositories.AbstractRepository):
-    def get_date_earliest_job_request_created(self):
-        return datetime.datetime(2025, 1, 1)
-
-    def get_date_latest_job_request_created(self):
-        return datetime.datetime(2025, 2, 1)
-
-    def get_job_requests(self, from_, to_):
-        return pandas.DataFrame(
-            {
-                "created_at": [
-                    datetime.datetime(2025, 1, 1),
-                    datetime.datetime(2025, 2, 1),
-                ],
-                "num_actions": [1, 1],
-                "num_jobs": [1, 1],
-                "username": ["a_user", "a_user"],
-            }
-        )
-
-    def get_jobs(self): ...  # pragma: no cover
+class FakeRepository(repositories.AbstractRepository): ...
 
 
 def test_app():
