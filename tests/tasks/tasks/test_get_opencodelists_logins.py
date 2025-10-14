0ba610519ba32e30b864383e895f553d1c446cc9
Iain Dillingham <iain@dillingham.me.uk>
2025-10-14 16:05:34 +0100

    Parametrize `test_get_records`
    This doesn't change the test; it moves the values of `last_login` and
    `login_at` into a parameter, such that we can add further parameters in
    a follow up commit.

--- tests/tasks/tasks/test_get_opencodelists_logins.py
+++ tests/tasks/tasks/test_get_opencodelists_logins.py
@@ -1,6 +1,8 @@
 import collections
 import datetime
 
+import pytest
+
 from tasks import utils
 from tasks.tasks import get_opencodelists_logins
 
@@ -8,14 +10,18 @@ from tasks.tasks import get_opencodelists_logins
 Row = collections.namedtuple("Row", ["last_login", "email"])
 
 
-def test_get_records():
-    row = Row(
-        last_login=datetime.datetime(2025, 1, 1, tzinfo=None),
-        email="user@example.com",
-    )
+@pytest.mark.parametrize(
+    "last_login,login_at",
+    [
+        (
+            datetime.datetime(2025, 1, 1, tzinfo=None),
+            datetime.datetime(2025, 1, 1, tzinfo=datetime.timezone.utc),
+        ),
+    ],
+)
+def test_get_records(last_login, login_at):
+    row = Row(last_login=last_login, email="user@example.com")
     records = list(get_opencodelists_logins.get_records([row]))
     record = records[0]
-    assert record.login_at == datetime.datetime(
-        2025, 1, 1, tzinfo=datetime.timezone.utc
-    )
+    assert record.login_at == login_at
     assert record.email_hash == utils.sha256("user@example.com")
