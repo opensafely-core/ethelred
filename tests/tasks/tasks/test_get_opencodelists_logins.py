651aee92b9ca2fbc66f778ff81f28c1009e2e645
Iain Dillingham <iain@dillingham.me.uk>
2025-10-15 06:36:07 +0100

    Revert "Parametrize `test_get_records`"
    This reverts commit cde21da92422ee6bc89c3e75d2f56d97bd1484bb.

--- tests/tasks/tasks/test_get_opencodelists_logins.py
+++ tests/tasks/tasks/test_get_opencodelists_logins.py
@@ -1,8 +1,6 @@
 import collections
 import datetime
 
-import pytest
-
 from tasks import utils
 from tasks.tasks import get_opencodelists_logins
 
@@ -10,18 +8,14 @@ from tasks.tasks import get_opencodelists_logins
 Row = collections.namedtuple("Row", ["last_login", "email"])
 
 
-@pytest.mark.parametrize(
-    "last_login,login_at",
-    [
-        (
-            datetime.datetime(2025, 1, 1, tzinfo=None),
-            datetime.datetime(2025, 1, 1, tzinfo=datetime.timezone.utc),
-        ),
-    ],
-)
-def test_get_records(last_login, login_at):
-    row = Row(last_login=last_login, email="user@example.com")
+def test_get_records():
+    row = Row(
+        last_login=datetime.datetime(2025, 1, 1, tzinfo=None),
+        email="user@example.com",
+    )
     records = list(get_opencodelists_logins.get_records([row]))
     record = records[0]
-    assert record.login_at == login_at
+    assert record.login_at == datetime.datetime(
+        2025, 1, 1, tzinfo=datetime.timezone.utc
+    )
     assert record.email_hash == utils.sha256("user@example.com")
