2d3bed6895dde9306f92e842651a960c6e29aa87
Iain Dillingham <iain@dillingham.me.uk>
2025-10-21 06:40:21 +0100

    Revert "Add a SQLite database for Ethelred data"
    This reverts commit 8f99951fb67d66de9a207501f783227b48fdb810.

--- tasks/db.py
+++ tasks/db.py
@@ -5,15 +5,12 @@ import sqlalchemy
 
 
 class Database(enum.StrEnum):
-    ETHELRED = enum.auto()
     JOBSERVER = enum.auto()
     OPENCODELISTS = enum.auto()
 
 
 def get_engine(database):
     match database:
-        case Database.ETHELRED:
-            return sqlalchemy.create_engine(os.environ["ETHELRED_DATABASE_URL"])
         case Database.JOBSERVER:
             return sqlalchemy.create_engine(os.environ["JOBSERVER_DATABASE_URL"])
         case Database.OPENCODELISTS:
