94434ca8dfe07b7e410627e30c620d508e9ba497
Iain Dillingham <iain@dillingham.me.uk>
2025-10-22 15:45:20 +0100

    Rename task

--- tests/tasks/tasks/test_get_opencodelists_login_events.py
+++ tests/tasks/tasks/test_get_opencodelists_login_events.py
@@ -2,7 +2,7 @@ import collections
 import datetime
 
 from tasks import utils
-from tasks.tasks import get_opencodelists_logins
+from tasks.tasks import get_opencodelists_login_events
 
 
 Row = collections.namedtuple("Row", ["last_login", "email"])
@@ -13,7 +13,7 @@ def test_get_records():
         last_login=datetime.datetime(2025, 1, 1, microsecond=1),
         email="user@example.com",
     )
-    records = list(get_opencodelists_logins.get_records([row]))
+    records = list(get_opencodelists_login_events.get_records([row]))
     record = records[0]
     assert record.login_at == datetime.datetime(2025, 1, 1, microsecond=0)
     assert record.email_hash == utils.sha256("user@example.com")
