d3b9693266fe04f3161fa91079c3e272102fe583
Iain Dillingham <iain@dillingham.me.uk>
2025-10-27 11:23:54 +0000

    Remove ability to read/write pickle files
    We don't need this ability any more.

--- tasks/io.py
+++ tasks/io.py
@@ -1,26 +1,18 @@
 import csv
 import json
 import pathlib
-import pickle
 
 
 def write(obj, f_path):
     f_path = pathlib.Path(f_path)
     f_path.parent.mkdir(parents=True, exist_ok=True)
     match f_path.suffix:
-        case ".pickle":
-            _write_pickle(obj, f_path)
         case ".csv":
             _write_csv(obj, f_path)
         case ".json":
             _write_json(obj, f_path)
         case _:
             raise ValueError(f"Unsupported file type {f_path.suffix}")
-
-
-def _write_pickle(obj, f_path):
-    with f_path.open("wb") as f:
-        pickle.dump(obj, f)
 
 
 def _write_csv(records, f_path):
@@ -40,17 +32,10 @@ def _write_json(obj, f_path):
 def read(f_path):
     f_path = pathlib.Path(f_path)
     match f_path.suffix:
-        case ".pickle":
-            return _read_pickle(f_path)
         case ".json":
             return _read_json(f_path)
         case _:
             raise ValueError(f"Unsupported file type {f_path.suffix}")
-
-
-def _read_pickle(f_path):
-    with f_path.open("rb") as f:
-        return pickle.load(f)
 
 
 def _read_json(f_path):
