353db0f950ab648937f51088095e3e36c129d56c
Iain Dillingham <iain@dillingham.me.uk>
2025-10-30 12:59:20 +0000

    Unbind `_get_events_per_day`
    `_get_events_per_day` doesn't need to be a method of `Repository`
    because it doesn't need to access any state or behaviour. Let's make it
    a function.

--- app/repositories.py
+++ app/repositories.py
@@ -43,32 +43,33 @@ class Repository(AbstractRepository):
         return self._call(self.login_events_uri, "max", "login_at").date()
 
     def get_login_events_per_day(self, from_, to_):
-        return self._get_events_per_day(self.login_events_uri, "login_at", from_, to_)
+        return _get_events_per_day(self.login_events_uri, "login_at", from_, to_)
 
     def get_codelist_create_events_per_day(self, from_, to_):
-        return self._get_events_per_day(
+        return _get_events_per_day(
             self.codelist_create_events_uri, "created_at", from_, to_
         )
 
-    def _get_events_per_day(self, uri, col, from_, to_):
-        assert from_ <= to_
-        with duckdb.connect() as conn:
-            rel = conn.read_csv(uri)
-            event_at = duckdb.ColumnExpression(col)
-            rel = rel.filter(event_at >= from_)
-            rel = rel.filter(event_at <= to_)
-            rel = rel.select(event_at.cast(sqltypes.DATE).alias("event_on"))
-            rel = rel.order("event_on")
-            rel = rel.aggregate(
-                [
-                    duckdb.ColumnExpression("event_on").alias("date"),
-                    duckdb.FunctionExpression("count").alias("count"),
-                ],
-                "event_on",
-            )
 
-            events_per_day = rel.to_df()
+def _get_events_per_day(uri, col, from_, to_):
+    assert from_ <= to_
+    with duckdb.connect() as conn:
+        rel = conn.read_csv(uri)
+        event_at = duckdb.ColumnExpression(col)
+        rel = rel.filter(event_at >= from_)
+        rel = rel.filter(event_at <= to_)
+        rel = rel.select(event_at.cast(sqltypes.DATE).alias("event_on"))
+        rel = rel.order("event_on")
+        rel = rel.aggregate(
+            [
+                duckdb.ColumnExpression("event_on").alias("date"),
+                duckdb.FunctionExpression("count").alias("count"),
+            ],
+            "event_on",
+        )
 
-        # interpolate counts of zero for days without events
-        idx = pandas.date_range(from_, to_, freq="D", normalize=True, name="date")
-        return events_per_day.set_index("date").reindex(idx).fillna(0).reset_index()
+        events_per_day = rel.to_df()
+
+    # interpolate counts of zero for days without events
+    idx = pandas.date_range(from_, to_, freq="D", normalize=True, name="date")
+    return events_per_day.set_index("date").reindex(idx).fillna(0).reset_index()
